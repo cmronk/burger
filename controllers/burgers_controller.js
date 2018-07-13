a5afc7e565d3e21e68d54e02577596ff0d8976c4
cmronk <christinemronk@gmail.com>
2018-07-13 01:31:36 -0500

    updated styling, logic still needs work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,42 @@
 var express = require("express");
-var burger = require("./models/burger.js");
+var router = express.Router();
+var burger = require("../models/burger.js");
 
+router.get("/", function (req, res) {
+    res.redirect("/burgers");
+});
 
-var PORT = process.env.PORT || 8080;
+router.get("/burgers", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-var app = express();
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, false], function () {
+        res.redirect("/burgers");
+        // res.json({ id: result.insertId });
+    });
+});
 
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log(req.params.id);
+    // console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,10 +1,21 @@
-var orm = require("./config/orm.js");
+var orm = require("../config/orm.js");
 
-orm.selectAll();
-
-orm.insertOne();
-
-orm.updateOne();
-
-
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
 module.exports = burger;
