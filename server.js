0f19d675c8b89bde208db792738857d45630b85e
cmronk <christinemronk@gmail.com>
2018-07-13 15:56:53 -0500

    commented files and removed console.log

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
+// requiring dependencies for our app
 var express = require("express");
 var bodyParser = require("body-parser");
-// on cats youtube
-var methodOverride = require("method-override");
 
+// setting PORT to process.env.PORT for heroku else use 8080
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 app.use(express.static("public"));
-// from cats video
-// app.use(express.static(process.cwd() * "/public"));
-app.use(methodOverride("_method"));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
@@ -21,9 +18,11 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
+// requiring routers created in controllers
 var router = require("./controllers/burgers_controller.js");
 
 app.use(router);
+
 
 app.listen(PORT, function() {
     console.log("Burgers now listening at localhost:" + PORT);
